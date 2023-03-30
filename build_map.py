9c39227ec64e58ac4c19cdd1a4073ed2a5286390
Aurora <78878792+auroraap@users.noreply.github.com>
2023-03-30 15:24:20 +0200

    Add files via upload

---
+++ build_map.py
@@ -0,0 +1,27 @@
+import csv
+
+def build_city_graph(filename):
+    file = open(filename, "r")
+    map_data = list(csv.DictReader(file, delimiter=","))
+    file.close()
+
+    graph = {}
+
+    for data in map_data:
+        node = data["From"]
+
+        if node not in graph.keys():
+            graph[node] = {
+                "neighbors": [],
+                "distances": [],
+            }
+            
+        neighbor = data["To"]
+        distance = data["Distance"]
+
+        graph[node]["neighbors"].append(neighbor)
+        graph[node]["distances"].append(distance)
+
+    print("Number of nodes: {num_nodes}".format(num_nodes=len(graph)))
+
+    return graph
