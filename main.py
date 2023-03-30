a51554070e2bacfc40c74e82088ae7be99d101c7
Aurora Åsgård Pedersen <aurora@Auroras-MacBook-Air.local>
2023-03-30 16:21:42 +0200

    comments

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ def main() -> int:
     num_patients = 40
     num_doctors = 3
     patient_locations = random.choices(list(turkey_map.keys()), k=num_patients)
-    
-    print("######## MEDICAL AID ALLOCATION PROBLEM ########")
+
+    print("######## MEDICAL AID ALLOCATION OPTIMIZATION PROBLEM ########")
     print("Number of cities: {num_cities}".format(num_cities=num_cities))
     print("Number of doctors: {num_doctors}".format(num_doctors=num_doctors))
     print("Number of patients: {num_patients}".format(num_patients=num_patients))
-    print("Patient locations (repetition allowed):")
-    pprint(patient_locations)
+    print("Patient locations (repetition allowed): {patient_locations}".format(patient_locations=patient_locations))
+
+    # Run the different algorithms to get solutions for the paths of the 3 doctors
+    # Evaluate the solutions with respect to distance travelled in total
 
     return 0
 
