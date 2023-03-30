5a395446575985e1ceb7b0f73c298b317eb49515
Aurora Åsgård Pedersen <aurora@Auroras-MacBook-Air.local>
2023-03-30 16:14:19 +0200

    patient locations + numbers defined

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import sys
+import random
 from build_map import build_city_graph
+from pprint import pprint
 
 def main() -> int:
 
     turkey_map = build_city_graph("distances.csv")
     num_cities = len(turkey_map)
-
+    num_patients = 40
+    num_doctors = 3
+    patient_locations = random.choices(list(turkey_map.keys()), k=num_patients)
+    
+    print("######## MEDICAL AID ALLOCATION PROBLEM ########")
     print("Number of cities: {num_cities}".format(num_cities=num_cities))
+    print("Number of doctors: {num_doctors}".format(num_doctors=num_doctors))
+    print("Number of patients: {num_patients}".format(num_patients=num_patients))
+    print("Patient locations (repetition allowed):")
+    pprint(patient_locations)
 
     return 0
 
