ca5dd65494c1ef1fd1e0f7a624a8808afe4bfe77
Aurora Åsgård Pedersen <aurora@Auroras-MacBook-Air.local>
2023-03-31 14:16:42 +0200

    add function to calculate straight line distance between cities

--- graph_utils.py
+++ graph_utils.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import math
 from geopy.geocoders import Nominatim
 
 def build_graph(filename):
@@ -41,3 +42,35 @@ def get_coordinates(graph):
     with open('turkey_coordinates.json', 'w') as fp:
         json.dump(locations, fp)
 
+def city_distance_estimate(city1, city2, cities_json):
+    """ Uses Haversine formula to estimate distance between two cities.
+
+    Parameters:
+        city1: Name of city.
+        city2: Name of city.
+        cities_json: JSON containing cities, and their longitude and latitude
+
+    Returns:
+        Distance bewteen the two cities in km.
+    """
+    with open(cities_json) as json_file:
+        cities_dict = json.load(json_file)
+
+    earth_radius = 6371*(10**3)
+
+    lon1 = cities_dict[city1]["longitude"]
+    lon2 = cities_dict[city2]["longitude"]
+    lat1 = cities_dict[city1]["latitude"]
+    lat2 = cities_dict[city2]["latitude"]
+
+    phi1 = lat1 * math.pi/180
+    phi2 = lat2 * math.pi/180
+    dphi = (lat2-lat1) * math.pi/180
+    dlambda = (lon2-lon1) * math.pi/180
+
+    a = math.sin(dphi/2) * math.sin(dphi/2) + math.cos(phi1) * math.cos(phi2) * math.sin(dlambda/2) * math.sin(dlambda/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+    distance_estimate = earth_radius * c / 1000
+
+    return distance_estimate
