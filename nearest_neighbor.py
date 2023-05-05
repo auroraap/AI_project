b3da30586448f1dcd2abb6174169c0528f33ec96
Aurora Åsgård Pedersen <aurora@Auroras-MacBook-Air.local>
2023-05-05 08:17:07 +0200

    pseudocode for nearest neighbor search

---
+++ nearest_neighbor.py
@@ -0,0 +1,13 @@
+
+
+def nearest_neighbor_search(doctor_location, patient_list, remaining_patients = {}):
+    # while patient list is not empty
+        # if a neighboring city is a patient, go there
+        # else
+        #   find nearest patient
+        #   go to neighboring city with shortest distance estimate to patient
+        # store step in solution:
+        #   doctor_step = {"location": doctor_location, "step length": doctor_step_length}
+        # update doctor location
+    # return solution
+    return 0
